120c4948837643ec03a1e76e1ef0568a02f53ab8
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-11-04 01:47:01 +0330

    Update test.py

--- test.py
+++ test.py
@@ -7,27 +7,19 @@ phone = input("📱 Phone (Help: 98918467....): ").strip()
 try:
     response = client.sendCode("android", phone, send_type=True)
     code_hash = response['data']['phone_code_hash']
-except Exception:
-    try:
-        response = client.sendCode("web", phone)
-        code_hash = response['data']['phone_code_hash']
-    except Exception as e:
-        print(e)
-        exit()
+except Exception:exit()
 
 code = input("Code : ").strip()
 
 try:
-    login_response = client.signIn("android", phone, code_hash, code)
+    login_response = client.signIn("android", phone, code_hash, code,save="session")
     print("✅ Ok Login")
+    print(login_response)
 except Exception as e:
     print("❌ Error Login :", e)
     exit()
 try:
     result = client.register(platform="android", auths=login_response['Auth'], keys=login_response['Key'])
-    if result:
-        print("✅ Ok Regester")
-    else:
-        print("❌ Not Regester")
-except Exception as e:
-    print(e)
+    if result:print("✅ Ok Regester")
+    else:print("❌ Not Regester")
+except Exception as e:print(e)
